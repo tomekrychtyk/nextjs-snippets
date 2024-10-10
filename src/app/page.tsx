2537b309b7dfa7e545e4247e0ef5d2ebb291d238
Tomek Rychtyk <tomasz.rychtyk@delve.com>
2024-10-10 16:41:31 +0200

    Snippet list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
+import Link from "next/link";
 import { db } from "@/db";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        key={snippet.id}
+        className="flex justify-between items-center p-2 border rounded"
+        href={`/snippets/${snippet.id}`}
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link className="border p-2 rounded" href="/snippets/new">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
