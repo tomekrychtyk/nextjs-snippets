37f675a4b562334884ec6365b993b685682c78e1
Tomek Rychtyk <tomasz.rychtyk@delve.com>
2024-10-10 17:29:54 +0200

    Edit snippet page

---
+++ src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,21 @@
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+
+interface SnippetEditPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+  const id = Number(props.params.id);
+  const snippet = await db.snippet.findFirst({
+    where: { id },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>Editing snippet with title: {snippet.title}</div>;
+}
